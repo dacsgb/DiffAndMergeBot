41811a0c65b91bd08452f8bac5c4e1048c45bab0
braidan <lego head 259 AT google mail>
2026-02-07 22:57:54 -0500

    Remove subscriber.cpp Cleanu variable name for consistency

--- firmware/include/uros/subscriber.hpp
+++ firmware/include/uros/subscriber.hpp
@@ -5,7 +5,7 @@
 
 namespace uros {
 
-template<class MessageType>
+template<typename MessageType>
 class Subscriber : public MemberFunction {
 public:
     Subscriber<MessageType>(
@@ -13,12 +13,14 @@ public:
         const rosidl_message_type_support_t* typeSupport, 
         const char* topicName,
         rclc_executor_t* executor,
+        const rclc_executor_handle_invocation_t invocation=ON_NEW_DATA,
         const rmw_qos_profile_t* qosProfile=&rmw_qos_profile_default)
-        :   subscriber_(),
+        :   subscription_(),
             node_(node),
             typeSupport_(typeSupport),
             topicName_(topicName),
             executor_(executor),
+            invocation_(invocation)
             MemberFunction(qosProfile) {
     
         // Check node, type support, and topic name are not nullptrs
@@ -29,19 +31,20 @@ public:
     
     ~Subscriber() { RCLC_UNUSED(fini()); }
     
-    void setSubscriptionCallback(rclc_subscription_callback_t cb) { cb_ = cb; }
+    void setSubscriptionCallback(rclc_subscription_callback_with_context_t cb) { cb_ = cb; }
 
 protected:
-    rcl_subscription_t subscriber_;
+    rcl_subscription_t subscription_;
     rcl_node_t* node_;
     const rosidl_message_type_support_t* typeSupport_;
     const char* topicName_;
     rclc_executor_t* executor_;
+    const rclc_executor_handle_invocation_t invocation_;
     MessageType msg_;
 
     rcl_ret_t init() override {
         rcl_ret_t rc = rclc_subscription_init(
-            &subscriber_,
+            &subscription_,
             node_,
             typeSupport_,
             topicName_,
@@ -54,10 +57,10 @@ protected:
 
         rc = rclc_executor_add_subscription(
             executor_,
-            &subscriber_,
+            &subscription_,
             &msg_,
             cb_,
-            ON_NEW_DATA);
+            invocation_);
         if (rc != RCL_RET_OK) {
             // TODO: Handle error
             return rc; // Exit function
@@ -66,11 +69,10 @@ protected:
     }
 
     rcl_ret_t fini() override {
-        return rcl_subscription_fini(&subscriber_, node_);
+        return rcl_subscription_fini(&subscription_, node_);
     };
 
 private:
     rclc_subscription_callback_t cb_;
-    void execute(const void* msg_in, void*) { }
 };
 } // namespace uros
